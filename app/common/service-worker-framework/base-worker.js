b7ed149cb658aec5d137fc4034fbac4b5f2a8108
OldFresco <hinsonkojo@gmail.com>
2017-01-19 00:27:02 +0000

    Rename files, and add class entitires

--- app/common/service-worker-framework/base-worker.js
+++ app/common/service-worker-framework/base-worker.js
@@ -3,6 +3,24 @@ class BaseWorker {
     constructor(memory) {
         this.memory = memory;
     }
+
+    commitToMemory(event) {
+        this
+            .memory
+            .save(event);
+    }
+
+    recall(eventId) {
+        this
+            .memory
+            .getEvent(eventId);
+    }
+
+    recallLast() {
+        this
+            .memory
+            .getLatest();
+    }
 }
 
 export default BaseWorker;

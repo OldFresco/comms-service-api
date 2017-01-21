7abaaa4da7a57502b1f15151a7474727d5e32f57
OldFresco <hinsonkojo@gmail.com>
2017-01-21 01:51:39 +0000

    minor work

---
+++ app/services/food-order-processing/capability-modules/message-disambiguation/probabilistic-disambiguator.js
@@ -0,0 +1,3 @@
+// takes in input message
+// tries to match input with some unambiguous 'sentiment'
+// spits out sentiment
---
+++ app/services/food-order-processing/capability-modules/message-disambiguation/reflex.js
@@ -0,0 +1,5 @@
+const defineReflexInputIntentMapping = () => {
+    return {'Menu?': 'VIEW_MENU', 'Open?': 'VIEW_OPENING_TIMES', 'Wwit': 'VIEW_CURRENT_WAITING_TME'}
+};
+
+export default defineReflexInputIntentMapping;

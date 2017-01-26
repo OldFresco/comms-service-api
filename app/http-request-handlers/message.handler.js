0ca722384a95107ffb4cf4e2991eb31fb9752ab2
Kojo Hinson <kojo.hinson@oakbrook.local>
2017-01-26 23:01:01 +0000

    Add memory and conversation pseudocode to message handler

--- app/http-request-handlers/message.handler.js
+++ app/http-request-handlers/message.handler.js
@@ -2,25 +2,34 @@ require('es6-promise').polyfill()
 
 import BaseHandler from './base.handler'
 import Waiter from '../service-modules/food-order-processing/workers/waiter'
-import disambiguate from '../service-modules/food-order-processing/capability-modules/message-disambiguation/deterministic-disambiguator'
+import disambiguate from '../service-modules/food-order-processing/capability-modules/message-disambiguati' +
+  'on/deterministic-disambiguator'
+import BaseConversation from '../shared-service-modules/conversation-framework/base-conversation'
 
 class MessageHandler extends BaseHandler {
-  constructor () {
+  constructor() {
     super()
     this.processMessage = this
       .processMessage
       .bind(this)
   }
 
-  processMessage (req, res) {
+  processMessage(req, res) {
     const message = req.body.Body
-    const brain = {}
+    const senderId = req.body.senderId
+    let brain = {}
 
     brain.disambiguate = disambiguate
 
     let waiter = new Waiter(null, brain)
-    let outcome = waiter.actOnMessage(message)
-    
+    let convo = waiter.recognizesSender(senderId)
+      ? waiter.recallConvesation(senderId)
+      : new BaseConversation([
+        'waiter', senderId
+      ], 'NEW_CONVO')
+
+    let outcome = waiter.actOnMessage(message, convo)
+
     res.json({response: outcome})
   }
 }
